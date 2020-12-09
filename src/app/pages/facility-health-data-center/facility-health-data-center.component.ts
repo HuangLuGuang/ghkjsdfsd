2e5e1234afd28903482ba4d17db542fb78ab6f6d
Zuoxioaxian <1980179070@qq.com>
2020-12-09 12:04:18 +0800

    设备报表-历史报警

--- src/app/pages/facility-health-data-center/facility-health-data-center.component.ts
+++ src/app/pages/facility-health-data-center/facility-health-data-center.component.ts
@@ -5,6 +5,7 @@ import { facility_health_SETTINGS } from './facility_health_data_center_table';
 import {LocalDataSource} from "@mykeels/ng2-smart-table";
 import { UserInfoService } from '../../services/user-info/user-info.service';
 import { HttpserviceService } from '../../services/http/httpservice.service';
+import { PublicmethodService } from '../../services/publicmethod/publicmethod.service';
 
 @Component({
   selector: 'ngx-facility-health-data-center',
@@ -12,7 +13,7 @@ import { HttpserviceService } from '../../services/http/httpservice.service';
   styleUrls: ['./facility-health-data-center.component.scss']
 })
 export class FacilityHealthDataCenterComponent implements OnInit {
-  @ViewChild("mytable") mytable: any;
+  @ViewChild("ag_Grid") agGrid: any;
   @ViewChild("eimdevicetpye") eimdevicetpye: any; // 设备类型下拉框
   @ViewChild("myinput") myinput: any; // 资产编号输入框
   @ViewChild("data_range") data_range: any; // 日期选择器
@@ -59,6 +60,54 @@ export class FacilityHealthDataCenterComponent implements OnInit {
   refresh = false; // 刷新tabel
   button; // 权限button
 
+  // =================================================agGrid
+
+  tableDatas = {
+    totalPageNumbers: 0, // 总页数
+    PageSize: 10, // 每页 10条数据
+    isno_refresh_page_size: false, // 是否重新将 每页多少条数据，赋值为默认值
+    columnDefs:[ // 列字段 多选：headerCheckboxSelection checkboxSelection , flex: 1 自动填充宽度  pinned: 'left' 固定在左侧！
+      { field: 'id', headerName: '序号', headerCheckboxSelection: true, checkboxSelection: true, autoHeight: true, fullWidth: true, minWidth: 50,resizable: true,},
+      { field: 'deviceName', headerName: '设备名称',  resizable: true, minWidth: 10},
+      { field: 'devicePosition', headerName: '设备位置', resizable: true, minWidth: 10}, // 自定义设备编号！
+      { field: 'alertContent', headerName: '报警内容', resizable: true, minWidth: 10},
+      { field: 'alertTime', headerName: '报警时间', resizable: true},
+      { field: 'status', headerName: '状态', resizable: true, minWidth: 10},
+      { field: 'handleTime', headerName: '处理时间', resizable: true, minWidth: 10},
+    ],
+    rowData: [ // data
+    ]
+  };
+
+  // 模拟数据
+  message = [
+    { id: 1, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "严重报警",  operation: "确认" },
+    { id: 2, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 3, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 4, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "报警",  operation: "确认" },
+    { id: 5, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 6, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "报警",  operation: "确认" },
+    { id: 7, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 8, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "严重报警",  operation: "确认" },
+    { id: 9, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 10, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 11, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 12, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 13, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 14, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 15, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "严重报警",  operation: "确认" },
+    { id: 16, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 17, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 18, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 19, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 20, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 21, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 22, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
+    { id: 23, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "严重报警",  operation: "确认" },
+  ];
+
+  private gridData = [];
+  
   source:LocalDataSource
   // 设备KPI统计 table数据
   table_data = {
@@ -67,6 +116,7 @@ export class FacilityHealthDataCenterComponent implements OnInit {
   };
 
   constructor(private userinfo:UserInfoService,private http:HttpserviceService,
+    private publicservice:PublicmethodService
 
   ) { 
     // 会话过期
@@ -76,7 +126,16 @@ export class FacilityHealthDataCenterComponent implements OnInit {
 
   }
 
-  ngOnInit(): void {   // 初始化table
+  ngOnInit(): void {   
+
+    // 得到pathname --在得到button
+    var roleid = this.userinfo.getEmployeeRoleID();
+    this.publicservice.get_buttons_bypath(roleid).subscribe(result=>{
+      this.button = result;
+      localStorage.setItem("buttons_list", JSON.stringify(result));
+    })
+
+    // 初始化table
     this.table_data.source["data"] = [
       { id: 1, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "严重报警",  operation: "确认" },
       { id: 2, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
@@ -102,6 +161,12 @@ export class FacilityHealthDataCenterComponent implements OnInit {
       { id: 22, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "普通",  operation: "确认" },
       { id: 23, deparrtment: "结构试验室", deviceName: "四立柱", devicePosition: "结构试验室", alertContent: "油温超限", alertTime: "2020-01-20 14:09:08", status: "已推送处理", handleTime: "2020-01-20 14:09:40", alertGrade: "严重报警",  operation: "确认" },
     ]
+  }
+
+  ngAfterViewInit(){
+    // 初始化table
+    this.inttable();
+    this.loading = false;
   }
 
   // button按钮
@@ -133,7 +198,12 @@ export class FacilityHealthDataCenterComponent implements OnInit {
   refresh_table(){
     this.refresh = true;
     this.loading = true;
-    // this.gridData = [];
+    this.gridData = [];
+    // 是否 每页多少也，设置为默认值
+    this.tableDatas.isno_refresh_page_size = true;
+    this.inttable();
+    this.loading = false;
+    this.refresh = false;
 
     // 取消选择的数据 delselect
     this.myinput.del_input_value();
@@ -189,8 +259,88 @@ export class FacilityHealthDataCenterComponent implements OnInit {
 
   // 导出文件
   download(title){
-    // this.agGrid.download(title);
+    this.agGrid.download(title);
   };
+
+  // =================================================agGrid
+   // 初始化table
+   inttable(event?){
+    var offset;
+    var limit;
+    var PageSize;
+    if (event != undefined){
+      offset = event.offset;
+      limit = event.limit;
+      PageSize = event.PageSize? Number(event.PageSize):10;
+    }else{
+      offset = 0;
+      limit = 10;
+      PageSize = 10;
+    }
+    var columns = {
+      offset: offset, 
+      limit: limit,
+    }
+    this.loading = true
+    var message = this.message;
+    this.tableDatas.PageSize = PageSize;
+    this.gridData.push(...message)
+    this.tableDatas.rowData = this.gridData;
+    var totalpagenumbers = this.message.length;
+    this.tableDatas.totalPageNumbers = totalpagenumbers;
+    setTimeout(() => {
+      this.agGrid.init_agGrid(this.tableDatas); // 告诉组件刷新！
+    }, 1000);
+    // 刷新table后，改为原来的！
+    this.tableDatas.isno_refresh_page_size = false;
+  }
+
+  // 更新table
+  update_agGrid(event?){
+    // 是否 每页多少也，设置为默认值
+    this.tableDatas.isno_refresh_page_size = true;
+    var offset;
+    var limit;
+    var PageSize;
+    if (event != undefined){
+      offset = event.offset;
+      limit = event.limit;
+      PageSize = event.PageSize? Number(event.PageSize):10;
+    }else{
+      offset = 0;
+      limit = 10;
+      PageSize = 10;
+    }
+    var columns = {
+      offset: offset, 
+      limit: limit,
+      employeeid: this.userinfo.getEmployeeID(),
+      devicename: '',
+      eimdevicetype: [], // 设备类型，可选
+      group: []          // 科室/功能组，可选
+    }
+
+    this.loading = false
+    var message = this.message;
+    this.tableDatas.PageSize = PageSize;
+    this.gridData.push(...message)
+    this.tableDatas.rowData = this.gridData;
+    var totalpagenumbers = this.message.length;
+    this.tableDatas.totalPageNumbers = totalpagenumbers;
+    setTimeout(() => {
+      this.agGrid.init_agGrid(this.tableDatas); // 告诉组件刷新！
+    }, 1000);
+    // 刷新table后，改为原来的！
+    this.tableDatas.isno_refresh_page_size = false;
+  }
+
+  // nzpageindexchange 页码改变的回调
+  nzpageindexchange_ag(event){
+    this.gridData = [];
+    this.loading = true;
+    this.inttable(event);
+    this.loading = false;
+  }
 
 
 }
--- src/app/pages/facility-health-data-center/facility-health-data-center.module.ts
+++ src/app/pages/facility-health-data-center/facility-health-data-center.module.ts
@@ -7,16 +7,19 @@ import { FacilityHealthDataCenterComponent } from './facility-health-data-center
 
 // import { Ng2SmartTableModule } from 'ng2-smart-table';
 import {Ng2SmartTableModule} from '@mykeels/ng2-smart-table';
-import { NbButtonModule, NbCardModule, NbIconModule, NbSpinnerModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbIconModule, NbSelectModule, NbSpinnerModule } from '@nebular/theme';
 import { MySelectTreeComponent } from './components/my-select-tree/my-select-tree.component';
 import { MySelectTreeTypeComponent } from './components/my-select-tree-type/my-select-tree-type.component';
 import { MyInputComponent } from './components/my-input/my-input.component';
 import { MyDateRangeComponent } from './components/my-date-range/my-date-range.component';
 import { FormsModule } from '@angular/forms';
+import { AgTableComponent } from './components/ag-table/ag-table.component';
+import { NzPaginationModule } from 'ng-zorro-antd';
+import { AgGridModule } from 'ag-grid-angular';
 
 
 @NgModule({
-  declarations: [FacilityHealthDataCenterComponent, MySelectTreeComponent, MySelectTreeTypeComponent, MyInputComponent, MyDateRangeComponent, 
+  declarations: [FacilityHealthDataCenterComponent, MySelectTreeComponent, MySelectTreeTypeComponent, MyInputComponent, MyDateRangeComponent, AgTableComponent, 
   ],
   imports: [
     CommonModule,
@@ -27,6 +30,11 @@ import { FormsModule } from '@angular/forms';
     NbIconModule,
     FormsModule,
     NbSpinnerModule,
+
+    // agtable
+    NzPaginationModule,
+    AgGridModule,
+    NbSelectModule,
 
   ],
 
